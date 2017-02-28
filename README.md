c38bffe0db7d5e43ca4be2219dbf48fb08bd5369
xhrix <xxxhrixxx@gmail.com>
2017-02-28 09:51:15 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Learning Travis With Node
---
+++ test/test.js
@@ -0,0 +1,12 @@
+const should = require('chai').should();
+
+describe('My Test Suite', function () {
+    it('#yeahFunction', function () {
+        'hi'.should.have.lengthOf(22);
+    });
+
+    it('#omgFunction', function () {
+        const nonExistant = void 0;
+        should.not.exist(nonExistant);
+    });
+});
